67a4c664a95fc7ed4dede8797cfcd96ca43b5abe
Adam Laimana <alaimana@yahoo.com>
2015-11-10 12:30:15 -1000

    -added  X and Y coord inputs in html -hard coded location values (work, home, etc.)

--- interface.js
+++ interface.js
@@ -1,58 +1,49 @@
 /**
  * Created by SlyMongoose on 11/8/2015.
  */
-var website;
-var cycletime;
-var range;
-
-console.log("hello");
-
-var getsite = function () {
-    website = prompt("Please enter a website");
-}
-
-var getcycle = function () {
-    cycletime = prompt("Please enter cycle 	time");
-}
-
-var getrange = function () {
-    range = prompt("Please enter a range");
-}
 
 var myVar = setInterval(myTimer, 5000);
 
 function myTimer() {
     var d = new Date();
-    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+    document.getElementById("answer").innerHTML = d.toLocaleTimeString();
 }
 
-var x = document.getElementById("demo");
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
+//var x = document.getElementById("demo");
+//
+//function getGeoLocation() {
+//    if (navigator.geolocation) {
+//        navigator.geolocation.getCurrentPosition(showPosition);
+//    } else {
+//        x.innerHTML = "Geolocation is not supported by this browser.";
+//    }
+//}
 
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude +
         "<br>Longitude: " + position.coords.longitude;
 }
-getsite();
-getcycle();
-getrange();
 
-document.write(website + " " + cycletime + " " + range);
+var homeX = 21.294846;
+var homeY = -157.813323;
+var workX = 21.297614;
+var workY = -157.841039;
+var schoolX = 21.299940;
+var schoolY = -157.819161;
+var queensX = 21.308444;
+var queensY = -157.853346;
+var kaiserX = 21.363877;
+var kaiserY = -157.899807;
+var dangerX = 21.262820;
+var dangerY = -157.824653;
+
+function getLocation(userX, userY) {
+    var a = document.getElementById("xCoord").value - userX;
+    var b = document.getElementById("yCoord").value - userY;
+
+    var c = Math.sqrt( a*a + b*b );
+
+    document.getElementById("answer").innerHTML = document.getElementById("xCoord").value;
+}
 
 
-//Mark Bercasio: var a = x1 - x2
-//var b = y1 - y2
-//
-//var c = Math.sqrt( a*a + b*b );
-//limit > number
-//color black
-//for input
-//we need user location
-//current user gps
