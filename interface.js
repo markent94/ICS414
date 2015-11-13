c86fe794fc64d066668cc430b65a780c590effec
Adam Laimana <alaimana@yahoo.com>
2015-11-12 21:55:16 -1000

    cleaned up code

--- interface.js
+++ interface.js
@@ -2,28 +2,6 @@
  * Created by SlyMongoose on 11/8/2015.
  */
 
-var myVar = setInterval(myTimer, 5000);
-
-function myTimer() {
-    var d = new Date();
-    document.getElementById("answer").innerHTML = d.toLocaleTimeString();
-}
-
-//var x = document.getElementById("demo");
-//
-//function getGeoLocation() {
-//    if (navigator.geolocation) {
-//        navigator.geolocation.getCurrentPosition(showPosition);
-//    } else {
-//        x.innerHTML = "Geolocation is not supported by this browser.";
-//    }
-//}
-
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-}
-
 var homeX = 21.294846;
 var homeY = -157.813323;
 var workX = 21.297614;
@@ -37,13 +15,50 @@ var kaiserY = -157.899807;
 var dangerX = 21.262820;
 var dangerY = -157.824653;
 
-function getLocation(userX, userY) {
-    var a = document.getElementById("xCoord").value - userX;
-    var b = document.getElementById("yCoord").value - userY;
+function getInteger(xCoord, yCoord) {
+    var a = xCoord;
+    var b = yCoord;
 
-    var c = Math.sqrt( a*a + b*b );
+    var c = Math.sqrt(Math.pow(a-homeX,2)+ Math.pow(b-homeY,2));
+    c = Math.sqrt(Math.pow(a-workX,2)+ Math.pow(b-workY,2));
+    c = Math.sqrt(Math.pow(a-schoolX,2)+ Math.pow(b-schoolY,2));
+    c = Math.sqrt(Math.pow(a-queensX,2)+ Math.pow(b-queensY,2));
+    c = Math.sqrt(Math.pow(a-kaiserX,2)+ Math.pow(b-kaiserY,2));
+    c = Math.sqrt(Math.pow(a-dangerX,2)+ Math.pow(b-dangerY,2));
 
-    document.getElementById("answer").innerHTML = document.getElementById("xCoord").value;
+    if(c < 1) {
+
+        document.getElementById("answer").innerHTML = "1";
+        return 1;
+    }
+    else
+    //else {
+    //    c = Math.sqrt(Math.pow(a-workX,2)+ Math.pow(b-workY,2));
+    //    if(c <1) {
+    //
+    //    }
+    //    else {
+    //
+    //    }
+    //}
+
+
+    document.getElementById("answer").innerHTML = c;
 }
 
+function getCurrentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        document.getElementById("answer").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    getInteger(position.coords.latitude, position.coords.longitude);
+}
+
+function getNewLocation() {
+    getInteger(document.getElementById("xCoord").value, document.getElementById("yCoord").value);
+}
 
